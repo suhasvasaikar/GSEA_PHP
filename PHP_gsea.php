6a5bc5b5b6378a62a79a211b0084f3454cfc8dbf
Suhas Vasaikar <suhas_be@student.iitd.ac.in>
2017-04-30 15:06:21 -0500

    Update PHP_gsea.php

--- PHP_gsea.php
+++ PHP_gsea.php
@@ -9,11 +9,11 @@
 
 $begin_time=microtime(true);
 
-$c_text='java -cp gsea2.jar -Xmx2048m xtools.gsea.GseaPreranked -gmx c5.all.v6.0.symbols.gmt -collapse false -mode Max_probe -norm meandiv -nperm 100 -rnk test_expressionfile.rnk -scoring_scheme weighted -rpt_label gsea_result -include_only_symbols true -make_sets true -plot_top_x 20 -rnd_seed timestamp -set_max 500 -set_min 15 -zip_report false -out GSEA_PHP-master/ -gui false';
+$c_text='java -cp gsea2.jar -Xmx2048m xtools.gsea.GseaPreranked -gmx c5.all.v6.0.symbols.gmt -collapse false -mode Max_probe -norm meandiv -nperm 100 -rnk test_expressionfile.rnk -scoring_scheme weighted -rpt_label gsea_result -include_only_symbols true -make_sets true -plot_top_x 20 -rnd_seed timestamp -set_max 500 -set_min 15 -zip_report false -out Output/ -gui false';
 
 $c_run=exec($c_text);
 
 $total_time=microtime(true)-$begin_time;
-echo "GSEA analysis is done. </br> 1. time".$total_time."</br>2. Result output (in same folder)";
+echo "GSEA analysis is done. </br> 1. time".$total_time."</br>2. Result output (in Output folder)";
 ?>
 
